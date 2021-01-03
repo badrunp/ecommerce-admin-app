48a5449d896b69ed13337f6e204c0371c36a0084
badrun <bbadrunn@gmail.com>
2021-01-03 20:13:03 +0700

    m

--- src/configs/urlConfigs.js
+++ src/configs/urlConfigs.js
@@ -1,5 +1,5 @@
 
-const urlPath = location.hostname === "localhost" ? "http://localhost:4000" : "https://duosdev-rest-server.herokuapp.com"
+const urlPath = window.location.hostname === "localhost" ? "http://localhost:4000" : "https://duosdev-rest-server.herokuapp.com"
 
 
 export const baseUrl = urlPath;
