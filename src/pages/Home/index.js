51a29ac70213d64226431e15d9ac3d75332dff2f
badrun <bbadrunn@gmail.com>
2021-01-09 11:00:32 +0700

    m

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "../../components/Layout";
-import io from "socket.io-client";
-import { baseUrl } from "../../configs/urlConfigs";
-import { useDispatch, useSelector } from "react-redux";
-import { getUserOnline } from "../../actions";
 
 function Home() {
-  const auth = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const { userOnline } = useSelector((state) => state.userOnline);
-
-  useEffect(() => {
-    let server = baseUrl;
-    let socket = io(server);
-
-    userOnline.map((item) => {
-      if (item.userId !== auth.user._id && item.room !== "global") {
-        socket.emit("joinRoom", {
-          name: auth.user.fullName,
-          room: "global",
-          userId: auth.user._id,
-        });
-
-        socket.on("usersList", ({ users }) => {
-          dispatch(getUserOnline(users));
-        });
-      }
-    });
-  }, [auth, userOnline]);
-
   return (
     <>
       <Layout>
