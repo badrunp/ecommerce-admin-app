525e55562543fdf489edfb549de1f6a3cf37054e
badrun <bbadrunn@gmail.com>
2021-01-20 09:17:25 +0700

    m

--- src/App.js
+++ src/App.js
@@ -21,6 +21,54 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // let server = baseUrl;
+    if (auth.authenticate) {
+      let socket = io(baseUrl, {
+        reconnection: true,
+        reconnectionAttempts: Infinity,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        randomizationFactor: 0.5,
+        rememberUpgrade: true,
+        transports: ["websocket"],
+        secure: true,
+        timeout: 50000,
+        pingTimeout: 50000,
+        autoConnect: true,
+        rejectUnauthorized: false,
+        auth: {
+          token: localStorage.getItem("token")
+            ? localStorage.getItem("token")
+            : "",
+        },
+      });
+
+      socket.on("connect", () => {
+        socket.emit("joinRoom", {
+          name: auth.user.fullName,
+          room: "global",
+          userId: auth.user._id,
+        });
+
+        let count = 0;
+        setInterval(() => {
+          socket.volatile.emit("ping", ++count);
+        }, 1000);
+
+        socket.on("usersList", ({ users }) => {
+          dispatch(getUserOnline(users));
+        });
+      });
+
+      socket.on("connect_error", () => {
+        setTimeout(() => {
+          socket.connect();
+        }, 1000);
+      });
+    }
+  }, [auth]);
+
+  useEffect(() => {
     if (!auth.authenticate) {
       dispatch(userIsLogin());
     }
@@ -32,75 +80,18 @@ function App() {
     if (auth.authenticate) {
       dispatch(getAllCategory());
     }
+    if (auth.authenticate) {
+      dispatch(getChat());
+    }
+
+    if (auth.authenticate) {
+      dispatch(getNotificationChats(auth.user._id));
+    }
   }, [auth.authenticate]);
 
   useEffect(() => {
     dispatch(checkDarkMode());
   }, [checkDarkMode]);
-
-  useEffect(() => {
-    if (auth.authenticate) {
-      dispatch(getNotificationChats(auth.user._id));
-    }
-  }, [auth]);
-
-  useEffect(() => {
-    if (auth.authenticate) {
-      dispatch(getChat());
-    }
-  }, [auth.authenticate]);
-
-  useEffect(() => {
-    // let server = baseUrl;
-    let socket = io(baseUrl, {
-      reconnection: true,
-      reconnectionAttempts: Infinity,
-      reconnectionDelay: 1000,
-      reconnectionDelayMax: 5000,
-      randomizationFactor: 0.5,
-      rememberUpgrade: true,
-      transports: ["websocket"],
-      secure: true,
-      timeout: 50000,
-      pingTimeout: 50000,
-      autoConnect: true,
-      rejectUnauthorized: false,
-      auth: {
-        token: localStorage.getItem("token")
-          ? localStorage.getItem("token")
-          : "",
-      },
-    });
-
-    socket.on("connect", () => {
-      socket.emit("joinRoom", {
-        name: auth.user.fullName,
-        room: "global",
-        userId: auth.user._id,
-      });
-
-      let count = 0;
-      setInterval(() => {
-        socket.volatile.emit("ping", ++count);
-      }, 1000);
-
-      socket.on("usersList", ({ users }) => {
-        dispatch(getUserOnline(users));
-      });
-    });
-
-    socket.on("connect_error", () => {
-      setTimeout(() => {
-        socket.connect();
-      }, 1000);
-    });
-
-    // socket = io({
-    //   auth: (cb) => {
-    //     cb(localStorage.getItem("token"));
-    //   },
-    // });
-  }, [auth]);
 
   return (
     <>
